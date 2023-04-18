822fcadefe5b82f7dc47a7a5e19ef8a2660b49fd
Mateusz_Ormianek <mattormianek@gmail.com>
2023-04-18 16:30:08 +0200

    Added quicksort in python and cython

--- Sorting algorithms/algorithms.pyx
+++ Sorting algorithms/algorithms.pyx
@@ -2,6 +2,7 @@
 #cython: language_level=3
 import cython
 import numpy as np
+from random import randint
 
 @cython.cdivision(True)
 @cython.boundscheck(False)
@@ -40,3 +41,34 @@ cpdef list insertion_sort_cython(list array):
     return array
 
 
+@cython.cdivision(True)
+@cython.boundscheck(False)
+cpdef list quicksort_cython(list array):
+    cdef list low, same, high
+    cdef int pivot
+    cdef int len_of_array = len(array)
+    cdef int item
+
+    if len_of_array < 2:
+        return array
+
+    low, same, high = [], [], []
+
+    # Select your `pivot` element randomly
+    pivot = array[randint(0, len_of_array - 1)]
+
+    for item in array:
+        # Elements that are smaller than the `pivot` go to
+        # the `low` list. Elements that are larger than
+        # `pivot` go to the `high` list. Elements that are
+        # equal to `pivot` go to the `same` list.
+        if item < pivot:
+            low.append(item)
+        elif item == pivot:
+            same.append(item)
+        elif item > pivot:
+            high.append(item)
+
+    # The final result combines the sorted `low` list
+    # with the `same` list and the sorted `high` list
+    return quicksort_cython(low) + same + quicksort_cython(high)
--- Sorting algorithms/app.py
+++ Sorting algorithms/app.py
@@ -2,7 +2,7 @@
 # create for every search comparsion in lineraity search
 from random import randint
 from timeit import repeat
-from algorithms import sorted_cython, bubble_sort_cython, insertion_sort_cython
+from algorithms import sorted_cython, bubble_sort_cython, insertion_sort_cython, quicksort_cython
 import time
 
 # template from real python
@@ -86,7 +86,34 @@ def insertion_sort_python(array):
 
     return array
 
+def quicksort_python(array):
+    # If the input array contains fewer than two elements,
+    # then return it as the result of the function
+    if len(array) < 2:
+        return array
 
+    low, same, high = [], [], []
+
+    # Select your `pivot` element randomly
+    pivot = array[randint(0, len(array) - 1)]
+
+    for item in array:
+        # Elements that are smaller than the `pivot` go to
+        # the `low` list. Elements that are larger than
+        # `pivot` go to the `high` list. Elements that are
+        # equal to `pivot` go to the `same` list.
+        if item < pivot:
+            low.append(item)
+        elif item == pivot:
+            same.append(item)
+        elif item > pivot:
+            high.append(item)
+
+    # The final result combines the sorted `low` list
+    # with the `same` list and the sorted `high` list
+    return quicksort_python(low) + same + quicksort_python(high)
+
+#ARRAY_LENGTH = 5000
 ARRAY_LENGTH = 5000
 
 if __name__ == "__main__":
@@ -102,3 +129,5 @@ if __name__ == "__main__":
     run_sorting_algorithm(algorithm="bubble_sort_python", array = array)
     run_sorting_algorithm(algorithm="insertion_sort_python", array = array)
     run_sorting_algorithm(algorithm="insertion_sort_cython", array = array)
+    run_sorting_algorithm(algorithm="quicksort_python", array = array)
+    run_sorting_algorithm(algorithm="quicksort_cython", array = array)
