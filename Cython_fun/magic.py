e721ea6ad302a7b92afa459c4c7582ff915fd7d5
Mateusz_Ormianek <mattormianek@gmail.com>
2023-06-21 16:31:21 +0200

    Created a plan for work and made it all functions

--- Cython_fun/magic.py
+++ Cython_fun/magic.py
@@ -1,66 +1,66 @@
+# USE: python3 magic.py <file>
 # Automate cythonize code
 import sys
 import re
-# take first argument as file in which look for automate cythonize
+# Plan:
+# Make all in functions
+# Write tests for already existing code
+# Write error catching
+# Check how to edit string and save it properly with indentation
+# Make list of rules how to change python code to cython code
+# Make list of types variables and how they change for cython
+
 file = sys.argv[1]
-
 print(f"Automate cythonize process on {file} file.")
-
-
-
-
-
 
 def find_function_end(text, start_line):
 
     code_lines = text.strip().split('\n')
-    indent = code_lines[start_line].split()[0]  # Get the indentation of the starting line
+    indent = code_lines[start_line].split()[0]
     end_line = start_line
 
     for line_number in range(start_line + 1, len(code_lines)):
         line = code_lines[line_number]
 
-        # Check if the line is indented with the same indentation as the starting line
         if line.startswith(indent):
-            end_line = line_number  # Update the end line
+            end_line = line_number
 
-        # Check if the line contains a 'return' statement
         if 'return' in line:
-            end_line = line_number  # Update the end line
-            break  # Exit the loop
+            end_line = line_number
+            break
 
     return end_line
 
+def magic(file):
+    # open file
+    with open(file, 'r') as file:
+        text = file.read()
+
+    #print(f"content_of_file: \n {text}")
+
+    word = '#acythonize'
+
+    lines = text.splitlines()
+    matches = []
+    for line_number, line in enumerate(lines):
+        if word.lower() in line.lower():
+            matches.append((line_number, line.index(word.lower())))
 
 
+    lines_of_function = []
+    for match in matches:
+        line_number, position = match
+        print(f"Found at line {line_number + 1}, position {position}: '{lines[line_number]}'")
+        lines_of_function.append(line_number+1)
 
 
-# open file
-with open(file, 'r') as file:
-    text = file.read()
+    for function in lines_of_function:
+        function_end_line = find_function_end(text, function)
 
-#print(f"content_of_file: \n {text}")
+        print(f"The functions starts at {function} and ends at {function_end_line+1}")
 
-word = '#acythonize'
-
-lines = text.splitlines()
-matches = []
-for line_number, line in enumerate(lines):
-    if word.lower() in line.lower():
-        matches.append((line_number, line.index(word.lower())))
+        print("Function code:")
+        print('\n'.join(lines[function:function_end_line+1]))
 
 
-lines_of_function = []
-for match in matches:
-    line_number, position = match
-    print(f"Found at line {line_number + 1}, position {position}: '{lines[line_number]}'")
-    lines_of_function.append(line_number+1)
-
-
-for function in lines_of_function:
-    function_end_line = find_function_end(text, function)
-
-    print(f"The functions starts at {function} and ends at {function_end_line+1}")
-
-    print("Function code:")
-    print('\n'.join(lines[function:function_end_line+1]))
+magic(file)
