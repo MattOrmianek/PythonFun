f64a8143f9cbd7e4f3f68dfe0862b992a7cc7c4b
Mateusz Ormianek <mattormianek@gmail.com>
2024-10-11 23:15:18 +0200

    feature: first stages of blog working with db

--- FastAPI/blog/main.py
+++ FastAPI/blog/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
-from blog.models import Blog
+from . import models
+from .database import engine, get_db
+from . import schemas
+from sqlalchemy.orm import Session
+from fastapi import Depends
+
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
+
 @app.post("/blog")
-def create_blog(blog: Blog):
-    return {'Creating: ': blog.title, 'body: ': blog.body}
+def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=blog.title, body=blog.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get("/blog")
+def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get("/blog/{id}")
+def get_blog(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
