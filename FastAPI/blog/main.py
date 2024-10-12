be09820c9032137972e2cad3712bfdbe17cee0a4
Mateusz Ormianek <mattormianek@gmail.com>
2024-10-12 10:30:39 +0200

    feature: added hashing, creating user

--- FastAPI/blog/main.py
+++ FastAPI/blog/main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException, Response
 from . import models
 from .database import engine, get_db
 from . import schemas
 from sqlalchemy.orm import Session
 from fastapi import Depends
-
+from .hashing import Hash
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
@@ -17,12 +17,42 @@ def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id: int, blog: schemas.Blog, db: Session = Depends(get_db)):
+    blog_to_update = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog_to_update.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog_to_update.update(dict(blog))
+    db.commit()
+    return Response(status_code=status.HTTP_202_ACCEPTED)
+
 @app.get("/blog")
 def get_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get("/blog/{id}")
+@app.get("/blog/{id}", response_model=schemas.Blog)
 def get_blog(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
     return blog
+
+@app.post("/user", status_code=status.HTTP_201_CREATED)
+def create_user(user: schemas.User, db: Session = Depends(get_db)):
+    hashed_password = Hash.bcrypt(user.password)
+    new_user = models.User(name=user.name, email=user.email, password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
--- FastAPI/blog/models.py
+++ FastAPI/blog/models.py
@@ -5,3 +5,10 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     body = Column(String, nullable=False)
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
