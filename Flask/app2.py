f064a7bfd92ad6d4b322d46779149329541e39eb
Mateusz Ormianek <mattormianek@gmail.com>
2024-10-13 15:11:56 +0200

    feature: added uploading files

--- Flask/app2.py
+++ Flask/app2.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, redirect, url_for
-
+import pandas as pd
+from lib.parser import read_excel
 app = Flask(__name__, template_folder="templates")
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -14,27 +16,47 @@ def index():
         else:
             return "Invalid credentials"
 
+@app.route("/file_upload", methods=["GET", "POST"])
+def file_upload():
+    if request.method == "GET":
+        return render_template("file_upload.html")
+    elif request.method == "POST":
+        file = request.files["file"]
+        if file and file.filename:
+            file.save(file.filename)
+            if file.filename.endswith(".xls") or file.filename.endswith(".xlsx"):
+                df = pd.read_excel(request.files["file"])
+                return df.to_html()
+            else:
+                return "File type not supported"
+        else:
+            return "No file uploaded"
+
+
 @app.route("/other")
 def other():
     myvalue = "Hello, World!"
     return render_template("other.html", myvalue=myvalue)
 
+
 @app.template_filter("reverse")
 def reverse_filter(s):
     return s[::-1]
 
+
 @app.template_filter("repeat")
 def repeat_filter(s, n):
-    return (s + ' ') * n
+    return (s + " ") * n
+
 
 @app.template_filter("alternate_case")
 def alternate_case_filter(s):
-    return ''.join([c.lower() if i % 2 == 0 else c.upper() for i,c in enumerate(s)])
+    return "".join([c.lower() if i % 2 == 0 else c.upper() for i, c in enumerate(s)])
 
-@app.route('/redirect')
+
+@app.route("/redirect")
 def redirect_to_other():
-    return redirect(url_for('other'))
-
+    return redirect(url_for("other"))
 
 
 if __name__ == "__main__":
